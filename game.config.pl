c89a5610540f7adbb8e4cdfd1837a5f612342d6c
Shaked Manes <maneshaked@gmail.com>
2020-10-20 00:51:15 +0300

    chore: Split the game into 2 modules - config and utils modules

---
+++ game.config.pl
@@ -0,0 +1,188 @@
+/*
+ * Author: Shaked Manes
+ * Purpose: Game Project for MAMAN 17
+ * Date: XX/10/2020
+ * Module: game.config.pl
+ * Description: Configurations module which used in the game project.
+ *
+ * This program was developed using SWI-Prolog, so it depends
+ * on the SWI-Prolog Prolog implementation standards.
+ *
+ * Also note that this programs requires SWI-Prolog font size of 36
+ * to run properly.
+ */
+:- module(
+       'game.config',
+       [
+           bone/2,
+           displayable_bone/2,
+           displayable_hand_bone/2,
+           displayable_player/2,
+           displayable_player_type/2
+       ]
+   ).
+
+/**
+ * Game roles predicates
+ */
+
+/** Bones tiles of the game **/
+bone(0, 0).
+
+bone(0, 1).
+bone(1, 1).
+
+bone(0, 2).
+bone(1, 2).
+bone(2, 2).
+
+bone(0, 3).
+bone(1, 3).
+bone(2, 3).
+bone(3, 3).
+
+bone(0, 4).
+bone(1, 4).
+bone(2, 4).
+bone(3, 4).
+bone(4, 4).
+
+bone(0, 5).
+bone(1, 5).
+bone(2, 5).
+bone(3, 5).
+bone(4, 5).
+bone(5, 5).
+
+bone(0, 6).
+bone(1, 6).
+bone(2, 6).
+bone(3, 6).
+bone(4, 6).
+bone(5, 6).
+bone(6, 6).
+
+/** Displayable bone tiles **/
+displayable_bone(bone(0, 0), "\uD83C\uDC31").
+displayable_bone(bone(0, 1), "\uD83C\uDC32").
+displayable_bone(bone(0, 2), "\uD83C\uDC33").
+displayable_bone(bone(0, 3), "\uD83C\uDC34").
+displayable_bone(bone(0, 4), "\uD83C\uDC35").
+displayable_bone(bone(0, 5), "\uD83C\uDC36").
+displayable_bone(bone(0, 6), "\uD83C\uDC37").
+
+displayable_bone(bone(1, 0), "\uD83C\uDC38").
+displayable_bone(bone(1, 1), "\uD83C\uDC39").
+displayable_bone(bone(1, 2), "\uD83C\uDC3A").
+displayable_bone(bone(1, 3), "\uD83C\uDC3B").
+displayable_bone(bone(1, 4), "\uD83C\uDC3C").
+displayable_bone(bone(1, 5), "\uD83C\uDC3D").
+displayable_bone(bone(1, 6), "\uD83C\uDC3E").
+
+
+displayable_bone(bone(2, 0), "\uD83C\uDC3F").
+displayable_bone(bone(2, 1), "\uD83C\uDC40").
+displayable_bone(bone(2, 2), "\uD83C\uDC41").
+displayable_bone(bone(2, 3), "\uD83C\uDC42").
+displayable_bone(bone(2, 4), "\uD83C\uDC43").
+displayable_bone(bone(2, 5), "\uD83C\uDC44").
+displayable_bone(bone(2, 6), "\uD83C\uDC45").
+
+displayable_bone(bone(3, 0), "\uD83C\uDC46").
+displayable_bone(bone(3, 1), "\uD83C\uDC47").
+displayable_bone(bone(3, 2), "\uD83C\uDC48").
+displayable_bone(bone(3, 3), "\uD83C\uDC49").
+displayable_bone(bone(3, 4), "\uD83C\uDC4A").
+displayable_bone(bone(3, 5), "\uD83C\uDC4B").
+displayable_bone(bone(3, 6), "\uD83C\uDC4C").
+
+displayable_bone(bone(4, 0), "\uD83C\uDC4D").
+displayable_bone(bone(4, 1), "\uD83C\uDC4E").
+displayable_bone(bone(4, 2), "\uD83C\uDC4F").
+displayable_bone(bone(4, 3), "\uD83C\uDC50").
+displayable_bone(bone(4, 4), "\uD83C\uDC51").
+displayable_bone(bone(4, 5), "\uD83C\uDC52").
+displayable_bone(bone(4, 6), "\uD83C\uDC53").
+
+displayable_bone(bone(5, 0), "\uD83C\uDC54").
+displayable_bone(bone(5, 1), "\uD83C\uDC55").
+displayable_bone(bone(5, 2), "\uD83C\uDC56").
+displayable_bone(bone(5, 3), "\uD83C\uDC57").
+displayable_bone(bone(5, 4), "\uD83C\uDC58").
+displayable_bone(bone(5, 5), "\uD83C\uDC59").
+displayable_bone(bone(5, 6), "\uD83C\uDC5A").
+
+displayable_bone(bone(6, 0), "\uD83C\uDC5B").
+displayable_bone(bone(6, 1), "\uD83C\uDC5C").
+displayable_bone(bone(6, 2), "\uD83C\uDC5D").
+displayable_bone(bone(6, 3), "\uD83C\uDC5E").
+displayable_bone(bone(6, 4), "\uD83C\uDC5F").
+displayable_bone(bone(6, 5), "\uD83C\uDC60").
+displayable_bone(bone(6, 6), "\uD83C\uDC61").
+
+displayable_hand_bone(hide_bone, "\uD83C\uDC62").
+
+displayable_hand_bone(bone(0, 0), "\uD83C\uDC63").
+displayable_hand_bone(bone(0, 1), "\uD83C\uDC64").
+displayable_hand_bone(bone(0, 2), "\uD83C\uDC65").
+displayable_hand_bone(bone(0, 3), "\uD83C\uDC66").
+displayable_hand_bone(bone(0, 4), "\uD83C\uDC67").
+displayable_hand_bone(bone(0, 5), "\uD83C\uDC68").
+displayable_hand_bone(bone(0, 6), "\uD83C\uDC69").
+
+displayable_hand_bone(bone(1, 0), "\uD83C\uDC6A").
+displayable_hand_bone(bone(1, 1), "\uD83C\uDC6B").
+displayable_hand_bone(bone(1, 2), "\uD83C\uDC6C").
+displayable_hand_bone(bone(1, 3), "\uD83C\uDC6D").
+displayable_hand_bone(bone(1, 4), "\uD83C\uDC6E").
+displayable_hand_bone(bone(1, 5), "\uD83C\uDC6F").
+displayable_hand_bone(bone(1, 6), "\uD83C\uDC70").
+
+displayable_hand_bone(bone(2, 0), "\uD83C\uDC71").
+displayable_hand_bone(bone(2, 1), "\uD83C\uDC72").
+displayable_hand_bone(bone(2, 2), "\uD83C\uDC73").
+displayable_hand_bone(bone(2, 3), "\uD83C\uDC74").
+displayable_hand_bone(bone(2, 4), "\uD83C\uDC75").
+displayable_hand_bone(bone(2, 5), "\uD83C\uDC76").
+displayable_hand_bone(bone(2, 6), "\uD83C\uDC77").
+
+displayable_hand_bone(bone(3, 0), "\uD83C\uDC78").
+displayable_hand_bone(bone(3, 1), "\uD83C\uDC79").
+displayable_hand_bone(bone(3, 2), "\uD83C\uDC7A").
+displayable_hand_bone(bone(3, 3), "\uD83C\uDC7B").
+displayable_hand_bone(bone(3, 4), "\uD83C\uDC7C").
+displayable_hand_bone(bone(3, 5), "\uD83C\uDC7D").
+displayable_hand_bone(bone(3, 6), "\uD83C\uDC7E").
+
+displayable_hand_bone(bone(4, 0), "\uD83C\uDC7F").
+displayable_hand_bone(bone(4, 1), "\uD83C\uDC80").
+displayable_hand_bone(bone(4, 2), "\uD83C\uDC81").
+displayable_hand_bone(bone(4, 3), "\uD83C\uDC82").
+displayable_hand_bone(bone(4, 4), "\uD83C\uDC83").
+displayable_hand_bone(bone(4, 5), "\uD83C\uDC84").
+displayable_hand_bone(bone(4, 6), "\uD83C\uDC85").
+
+displayable_hand_bone(bone(5, 0), "\uD83C\uDC86").
+displayable_hand_bone(bone(5, 1), "\uD83C\uDC87").
+displayable_hand_bone(bone(5, 2), "\uD83C\uDC88").
+displayable_hand_bone(bone(5, 3), "\uD83C\uDC89").
+displayable_hand_bone(bone(5, 4), "\uD83C\uDC8A").
+displayable_hand_bone(bone(5, 5), "\uD83C\uDC8B").
+displayable_hand_bone(bone(5, 6), "\uD83C\uDC8C").
+
+displayable_hand_bone(bone(6, 0), "\uD83C\uDC8D").
+displayable_hand_bone(bone(6, 1), "\uD83C\uDC8E").
+displayable_hand_bone(bone(6, 2), "\uD83C\uDC8F").
+displayable_hand_bone(bone(6, 3), "\uD83C\uDC90").
+displayable_hand_bone(bone(6, 4), "\uD83C\uDC91").
+displayable_hand_bone(bone(6, 5), "\uD83C\uDC92").
+displayable_hand_bone(bone(6, 6), "\uD83C\uDC93").
+
+/** Displaying players names **/
+displayable_player(player_one, 'Player 1').
+displayable_player(player_two, 'Player 2').
+
+/** Displaying players types **/
+displayable_player_type(user_player, 'You').
+displayable_player_type(computer_random, 'Computer Random Bot').
+displayable_player_type(computer_statistical, 'Computer Statistical Bot').
---
+++ game.utils.pl
@@ -0,0 +1,444 @@
+/*
+ * Author: Shaked Manes
+ * Purpose: Game Project for MAMAN 17
+ * Date: XX/10/2020
+ * Module: game.utils.pl
+ * Description: Utilities module which used in the game project.
+ *
+ * This program was developed using SWI-Prolog, so it depends
+ * on the SWI-Prolog Prolog implementation standards.
+ *
+ * Also note that this programs requires SWI-Prolog font size of 36
+ * to run properly.
+ */
+
+:- module(
+       'game.utils',
+       [
+           cut_wrapper/1,
+           remove_element_from_list/3,
+           matrix_dimensions/2,
+           generate_decision_matrix/1,
+           get_element_from_decision_matrix/4,
+           get_row_from_decision_matrix/3,
+           update_element_in_decision_matrix/4,
+           operation_on_element_in_list/5,
+           set_element_in_list/6,
+           member_diff_list/2,
+           list_length/2,
+           merge_sort/3
+       ]
+   ).
+
+/** Utilities **/
+
+/**
+ * cut_wrapper(Predicate):-
+ *   Wrap Predicate with cut to run the predicate only once without
+ *   backtracking.
+ *
+ *   INPUT:
+ *     P - Predicate to run with cut.
+ */
+cut_wrapper(Predicate):-
+    call(Predicate),
+    !.
+cut_wrapper(_).
+
+/**
+ * remove_element_from_list(Element, GivenList, ReturnedList):-
+ *   Removes Element given from the GivenList, and return the GivenList
+ *   without the Element in ReturnedList.
+ *
+ *   INPUT:
+ *     Element - Given element to remove from the GivenList.
+ *     GivenList - A list to search for the Element given.
+ *   OUTPUT:
+ *     ReturnedList - The GivenList without the Element.
+ */
+
+% Iterate over the list, when finding element which is not equal to the
+% given element, continue searching.
+remove_element_from_list(Element, [NotElement | RestList1], [NotElement | RestList2]):-
+    Element \= NotElement,
+    !,
+    remove_element_from_list(Element, RestList1, RestList2).
+
+% When finding the element in the list, remove it and keep gather all
+% rest elements.
+remove_element_from_list(Element, [Element | RestList1], RestList2):-
+    !,
+    remove_element_from_list(Element, RestList1, RestList2).
+
+% Empty list will return always empty list.
+remove_element_from_list(_, [], []).
+
+
+/** Decision Matrix Predicates **/
+
+% Decision matrix dimensions.
+matrix_dimensions(8, 7).
+
+/**
+ * generate_decision_matrix(Matrix):-
+ *   Generates a brand new empty decision matrix for the game.
+ *
+ *   OUTPUT:
+ *     Matrix - The new empty decision matrix that was generated.
+ */
+% This is the most efficient way to create matrix.
+generate_decision_matrix(Matrix):-
+    Matrix = [
+	[0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0]
+    ].
+
+/**
+ * get_element_from_decision_matrix(RowNum, ColNum, Matrix, Value):-
+ *   Retrieves a value by row number and column number in the given
+ *   matrix.
+ *
+ *   INPUT:
+ *     RowNum - The row number of the value to retrieve.
+ *     ColNum - The column number of the value to retrieve.
+ *     Matrix - The matrix which the value will be retrieved from.
+ *
+ *   OUTPUT:
+ *     Value - The value retreived from the matrix.
+ */
+
+% Get element at specific location at the matrix.
+% First get the row, and access the ColNum index in the selected row.
+get_element_from_decision_matrix(RowNum, ColNum, Matrix, Value):-
+    get_row_from_decision_matrix(RowNum, Matrix, SelectedRow),
+    nth0(ColNum, SelectedRow, Value).
+
+/**
+ * get_row_from_decision_matrix(RowNum, Matrix, RequestedRow):-
+ *   Retrieves a row from the matrix.
+ *
+ *   INPUT:
+ *     RowNum - The row number to retrieve from the matrix.
+ *     Matrix - The matrix which the row will be retrieved from.
+ *
+ *   OUPUT:
+ *     RequestedRow - The actual row which retrieved from the matrix.
+ */
+
+% Get specific row from matrix.
+% Access the RowNum index in the matrix.
+get_row_from_decision_matrix(RowNum, Matrix, RequestedRow):-
+    nth0(RowNum, Matrix, RequestedRow).
+
+
+/**
+ * update_element_in_decision_matrix(
+ *      RowNum,
+ *      ColNum,
+ *      Matrix,
+ *      NewMatrix
+ * ):-
+ *   Updates element in the decision matrix.
+ *   Basically it updates the appearence of bone tile in the matrix
+ *   by setting the corresponding row number and column number of the
+ *   bone tile to 1 in the matrix (also setting the opposite
+ *   combination - treat the row number as column and column as row,
+ *   as the bone tile can be also considered in reverse order).
+ *
+ *   After that, also updating the last row in the matrix
+ *   (which is also known as 'Probability Row').
+ *   The last row contains sum of the column values to indicates
+ *   the probability of each number in the tiles existing in the current
+ *   situation.
+ *
+ *   INPUT:
+ *     RowNum - Row number to update in the matrix.
+ *     ColNum - Column number to update in the matrix.
+ *     Matrix - The matrix to perform the update on.
+ *
+ *   OUTPUT:
+ *     NewMatrix - The updated matrix after the update operation.
+ */
+
+% Update element in decision matrix.
+update_element_in_decision_matrix(RowNum, ColNum, Matrix, NewMatrix):-
+    % Retrieve the matrix row size and calcualte the actual index.
+    matrix_dimensions(RowSize, _),
+    ActualLastRowIndex is RowSize - 1,
+    (
+	(
+            % Means this is a double bone tile
+            RowNum == ColNum,
+            !,
+
+            % Get the selected row, and the summary row from the matrix
+            get_row_from_decision_matrix(RowNum, Matrix, SelectedRow),
+            get_row_from_decision_matrix(ActualLastRowIndex, Matrix, SummaryRow),
+
+            % Update the value in the selected double tile (increase one)
+            operation_on_element_in_list(ColNum, 1, add, SelectedRow, UpdatedRow),
+
+            % Replace the updated row in the matrix
+            operation_on_element_in_list(RowNum, UpdatedRow, replace, Matrix, UpdatedMatrix),
+
+            % Update the summary row
+            operation_on_element_in_list(RowNum, 1, add, SummaryRow, UpdatedSummaryRow),
+
+            % Replace the summary row in the matrix
+            operation_on_element_in_list(ActualLastRowIndex, UpdatedSummaryRow, replace, UpdatedMatrix, NewMatrix)
+        )
+	;
+	(
+            % Get the corresponding bone tiles rows (Reversed and regular bone).
+            !,
+            get_row_from_decision_matrix(RowNum, Matrix, SelectedRow),
+            get_row_from_decision_matrix(ColNum, Matrix, SelectedCol),
+
+            % Get the summary row.
+            get_row_from_decision_matrix(ActualLastRowIndex, Matrix, SummaryRow),
+
+            % Update selected row and col.
+            operation_on_element_in_list(ColNum, 1, add, SelectedRow, UpdatedRow),
+            operation_on_element_in_list(RowNum, 1, add, SelectedCol, UpdatedCol),
+
+            % Update summary row with values appended in selected row and col.
+            operation_on_element_in_list(RowNum, 1, add, SummaryRow, UpdatedSummaryRow1),
+            operation_on_element_in_list(ColNum, 1, add, UpdatedSummaryRow1, UpdatedSummaryRow2),
+
+            % Update matrix with updated row, updated col, updated summary row.
+            operation_on_element_in_list(RowNum, UpdatedRow, replace, Matrix, UpdatedMatrix1),
+            operation_on_element_in_list(ColNum, UpdatedCol, replace, UpdatedMatrix1, UpdatedMatrix2),
+            operation_on_element_in_list(ActualLastRowIndex, UpdatedSummaryRow2, replace, UpdatedMatrix2, NewMatrix)
+        )
+    ).
+
+
+/**
+ * operation_on_element_in_list(Index, Value, Operation, List, NewList):-
+ *   Perform operation on specific index in list with given value
+ *   and return the result list.
+ *
+ *   INPUT:
+ *     Index - The element's index to perform operation on.
+ *     Value - The value to use in the operation.
+ *     Operation - The operation to perfrom to the element.
+ *                 One of the two:
+ *                 - add: adding the value to the element.
+ *                 - replace: replace the element with the value.
+ *     List - The list to perform the operation on.
+ *   OUTPUT:
+ *     NewList - The result list after the operation.
+ */
+operation_on_element_in_list(Index, Value, Operation, List, NewList):-
+    set_element_in_list(Index, 0, Value, Operation, List, NewList).
+
+
+/**
+ * set_element_in_list(
+ *   Index,
+ *   CurrIndex,
+ *   Value,
+ *   Operation,
+ *   List,
+ *   NewList
+ * ):-
+ *   Helper predicate for the `operation_on_element_in_list` predicate.
+ *
+ *   INPUT:
+ *     Index - The element's index to perform operation on.
+ *     CurrIndex - The current index reached.
+ *     Value - The value to use in the operation.
+ *     Operation - The operation to perfrom to the element.
+ *                 One of the two:
+ *                 - add: adding the value to the element.
+ *                 - replace: replace the element with the value.
+ *     List - The list to perform the operation on.
+ *
+ *   OUTPUT:
+ *     NewList - The result list after the operation.
+ *
+ */
+
+% Empty list, no-op performed.
+set_element_in_list(_, _, _, _, [], []):- !.
+
+% Reached other index than the requested index, continue iterating.
+set_element_in_list(
+    Index,
+    OtherIndex,
+    Value,
+    Operation,
+    [Element | List],
+    [Element | NewList]
+):-
+    OtherIndex \== Index,
+    !,
+    NextIndex is OtherIndex + 1,
+    set_element_in_list(Index, NextIndex, Value, Operation, List, NewList).
+
+% Reached the requested index, perform the operation and continue copy
+% the elements to the new list.
+set_element_in_list(
+    Index,
+    Index,
+    Value,
+    Operation,
+    [Element | List],
+    [OtherElement | NewList]
+):-
+    (
+	(
+            % Replace operation
+            Operation == replace,
+            !,
+            OtherElement = Value
+        )
+	;
+	(
+            % Add operation
+            OtherElement is Element + Value,
+            !
+        )
+    ),
+    NextIndex is Index + 1,
+    set_element_in_list(Index, NextIndex, Value, Operation, List, NewList).
+
+/**
+ * member_diff_list(Element, List-RestList):-
+ *   The usual member predicate, but for lists in format of
+ *   difference-lists.
+ */
+
+% The element found in the list.
+member_diff_list(Element, List-RestList):-
+    List \== RestList,
+    List = [Element | _].
+
+% The element did not found yet in the list, continue to rest of the
+% list.
+member_diff_list(Element, List-RestList):-
+    List \== RestList,
+    List = [_ | Tail],
+    member_diff_list(Element, Tail-RestList).
+
+/**
+ * list_length(List, Length):-
+ *   Returns the length of given list.
+ *
+ *   INPUT:
+ *     List - A list.
+ *
+ *   OUTPUT:
+ *     Length - The length of the given list.
+ */
+
+% Empty list, lenght of zero.
+list_length([], 0).
+
+% List contains element, count it and continue iterating.
+list_length([_ | Rest], Length):-
+    list_length(Rest, RestLength),
+    Length is RestLength + 1.
+
+/**
+ * merge_sort_util_merge(
+ *   Estimator,
+ *   MergedList,
+ *   List1,
+ *   List2
+ * ):-
+ *   Merge sort utility predicate for merging sorted lists.
+ *
+ *   INPUT:
+ *     Estimator - A predicate which estimate the 'greather-than'
+ *                 condition which defines the sort.
+ *     List1 - A list to merge.
+ *     List2 - A list to merge.
+ *
+ *   OUTPUT:
+ *     MergedList - The merged list which contains the two lists given
+ *                  sorted in one list.
+ */
+
+% One of the lists is empty, the merge is the actual filled list of
+% them.
+merge_sort_util_merge(_, List, List, []).
+merge_sort_util_merge(_, List, [], List).
+
+% First list has the element which is the greater one.
+merge_sort_util_merge(
+    Estimator,
+    [ElemList1 | RestMerged],
+    [ElemList1 | RestList1],
+    [ElemList2 | RestList2]
+):-
+    call(Estimator, ElemList1, ElemList2, ElemList1),
+    !,
+    merge_sort_util_merge(
+        Estimator,
+        RestMerged,
+        RestList1,
+        [ElemList2 | RestList2]
+    ),
+    !.
+
+% Second list has the element which is the greather one.
+merge_sort_util_merge(
+    Estimator,
+    [ElemList2 | RestMerged],
+    [ElemList1 | RestList1],
+    [ElemList2 | RestList2]
+):-
+    call(Estimator, ElemList2, ElemList1, ElemList2),
+    !,
+    merge_sort_util_merge(
+        Estimator,
+        RestMerged,
+        [ElemList1 | RestList1],
+        RestList2
+    ),
+    !.
+
+/**
+ * merge_sort(Estimator, List, Sorted):-
+ *   Merge sort predicate, which sorts a given list by given estimator
+ *   predicate which sets the condition of 'greather-than' to define
+ *   the sort order.
+ *
+ *   INPUT:
+ *     Estimator - A predicate which estimate the 'greather-than'
+ *                 condition which defines the sort.
+ *     List - A list to sort.
+ *
+ *   OUTPUT:
+ *     Sorted - The list given in sorted order.
+ */
+
+% Sort empty or one element lists, return the same list.
+merge_sort(_, [], []).
+merge_sort(_, [Element], [Element | []]).
+
+% Using official merge sort algorithm to sort the list.
+merge_sort(Estimator, List, Sorted):-
+    !,
+    length(List, N),
+    !,
+    FirstLength is N // 2,
+    SecondLength is N - FirstLength,
+    length(FirstUnsorted, FirstLength),
+    !,
+    length(SecondUnsorted, SecondLength),
+    !,
+    append(FirstUnsorted, SecondUnsorted, List),
+    !,
+    merge_sort(Estimator, FirstUnsorted, FirstSorted),
+    !,
+    merge_sort(Estimator, SecondUnsorted, SecondSorted),
+    !,
+    merge_sort_util_merge(Estimator, Sorted, FirstSorted, SecondSorted).
